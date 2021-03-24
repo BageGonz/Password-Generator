7bdfedaf7ac1a1f8d2f91fd6549ca7b6ad528e62
Bage Gonz <BageGonz@users.noreply.github.com>
2021-03-23 20:54:38 -0400

    Added character sets for the password to push

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 // Assignment Code
+var specCharacterSet = [
+  "!", "@", "#", "$", "%", "^", "&", "(", ")", "-", "]", "[", "{", "}", "||", "="
+];
+
+var numberSet = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+
+];
+
+var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"
+
+];
+
+var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z"
+
+];
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +31,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+
+  var passwordLength = prompt("Enter the length of you password");
+  //If statement to dertermine if length is between 8 and 128
+
+  var yesNumbers = confirm("Include Numbers ");
+  var yesSpecialChat = confirm("Includes special characters");
+  var yesUpperCase = confirm("Includes uppercase");
+  console.log(yesNumbers);
+
+  console.log(passwordLength);
+  return 
+}
